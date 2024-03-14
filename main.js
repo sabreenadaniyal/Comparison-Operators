e4e23586a98db88f05c9b083ac6ccd33aa6799a4
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-03-15 02:18:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict";
+//COMPARISION OPERATORS
+//Equal to(==):
+let a = 20;
+let b = 8;
+console.log(a == b);
+//Not Equalto(!==):
+let c = 18;
+let d = 5;
+console.log(c != d);
+//Equal to & data type(===):
+let e = 14;
+let f = "8";
+//@ts-ignore
+console.log(e === f);
+//Not equalto & data type(!==):
+let g = 25;
+let h = 25;
+console.log(g !== h);
+//Lessthan(<):
+let i = 5;
+let j = -50;
+console.log(i < j);
+//Greaterthan(>):
+let k = 45;
+let l = 30;
+console.log(k > l);
+//Lessthan & data type(<=):
+let m = 50;
+let n = 32;
+console.log(m <= n);
+//Greaterthan & data type(>=):
+let o = 180;
+let p = 150;
+console.log(o >= p);
---
+++ main.ts
@@ -0,0 +1,42 @@
+//COMPARISION OPERATORS
+//Equal to(==):
+let a:number=20
+let b:number=8
+console.log(a == b);  
+
+//Not Equalto(!==):
+let c:number=18
+let d:number=5
+console.log(c != d);
+
+//Equal to & data type(===):
+let e:number=14
+let f:string="8"
+//@ts-ignore
+console.log(e === f);
+
+//Not equalto & data type(!==):
+let g:number=25
+let h:number=25
+console.log(g !== h)
+
+//Lessthan(<):
+let i:number=5
+let j:number=-50
+console.log(i < j);
+
+//Greaterthan(>):
+let k:number=45
+let l:number=30
+console.log(k > l);
+
+//Lessthan & data type(<=):
+let m:number=50
+let n:number=32
+console.log(m <= n);
+
+//Greaterthan & data type(>=):
+let o:number=180
+let p:number=150
+console.log(o >= p);
+
